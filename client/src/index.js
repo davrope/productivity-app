17af8587bd82fe5cc2213d766e4807e7670a7cd1
David <rodpe.david@gmail.com>
2022-01-21 20:16:41 -0600

    Index in client and packages installed for it

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import axios from 'axios';
+window.axios = axios;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(reduxThunk)));
+
+ReactDOM.render(<Provider store = {store}><App/></Provider> , document.querySelector('#root'));
