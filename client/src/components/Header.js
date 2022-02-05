2cbc69d9297b878619c89ae1dab167e0152c5305
David <rodpe.david@gmail.com>
2022-02-04 19:37:23 -0600

    Back to desktop header version

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {FaBars} from 'react-icons/fa';
+import {FaTimes} from 'react-icons/fa'
 
 // Import libraries to make it responsive:
 
@@ -9,11 +10,14 @@ import styled from 'styled-components'
 
 class Header extends React.Component{
 
-
     renderContent(){
         switch(this.props.auth){
             case null:
-                return;
+                return(
+                  <div>
+                    Jeje algo salió mal
+                  </div>
+                );
             case false:
                     return (
                       <NavLinkS className = "google log in" href = "/auth/google" key = "googlelogin"> 
@@ -28,8 +32,6 @@ class Header extends React.Component{
                 ];
         }
     }
-
-
 
     // render() {
     //     return (
@@ -73,10 +75,10 @@ class Header extends React.Component{
               <i className='home icon'></i>
             </NavLogo>
 
-            <MobileIcon >
+            {/* <MobileIcon onClick={this.handleToggle()}>
             {/* <MobileIcon onClick={toggle}> */}
-                <FaBars/>
-            </MobileIcon>
+                {/* <FaBars/>
+            </MobileIcon> */}
             <NavMenu>
               <NavItem>
                 <NavLinkS className = "item" href= {this.props.auth ? '/projects' : '/'} key = "projects button">
@@ -99,6 +101,7 @@ class Header extends React.Component{
                 </div>
               </NavItem>
             </NavMenu>
+
 
 
           </NavbarContainer>
@@ -235,6 +238,88 @@ const NavLinkS = styled.a`
     &.active{
         border-bottom: 3px solid #00A3A3;
     }
+`
+
+
+
+// ***************************************************
+// ABOVE THIS THE STYLES ARE FOR MobileIcon
+// *****************************************************
+
+// const SidebarContainer = styled.aside`
+//     position: fixed;
+//     z-index:999;
+//     width: 100%;
+//     height: 100%;
+//     background: #0d0d0d;
+//     display: grid;
+//     align-items: center;
+//     top: 0;
+//     left: 0;
+//     transition: 0.3s ease-in-out;
+
+
+// `
+const CloseIcon = styled(FaTimes)`
+    color: #fff;
+`
+
+const Icon = styled.div`
+    position: absolute;
+    top: 1.2 rem;
+    right: 1.5 rem;
+    background: transparent;
+    font-size: 2rem;
+    cursor:pointer;
+    outline: none;
+
+`
+
+const SidebarWrapper = styled.div`
+    color: #fff;
+
+`
+// const SidebarMenu = styled.div`
+//     position: absolute;
+//     top: 1.2rem;
+//     right: 1.5rem;
+//     background: transparent;
+//     font-size: 2rem;
+//     cursor:pointer;
+//     outline: none;
+
+// `
+
+
+const SidebarMenu = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(6, 80px);
+    text-align: center;
+
+    @media screen and (max-width: 480px){
+        grid-template-rows: repeat(6, 60px);
+    }
+`
+
+
+const SidebarLink = styled.a`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    text-decoration: none;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover{
+        color:#27d594;
+        transition: 0.2s ease-in-out;
+    }
+
 `
 
 function mapStateToProps({ auth }) {
